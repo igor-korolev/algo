734107f21d9ae998abdb7c558036c8057f919ac9
Igor Korolev <in.earth@ya.ru>
2020-05-26 22:55:26 +0300

    Fix recursive binary search and add tests

--- search/binary_search.py
+++ search/binary_search.py
@@ -1,30 +1,51 @@
-# TODO: add docs and reformat
+"""
+Binary search is a search algorithm that finds the position of a target value within a sorted
+array. Binary search runs in logarithmic time O(log n)
+"""
+import random
 
 
-def bin_search(array, value):
-    if not array:
-        return None
-    lenght = len(array)
-    half = lenght // 2
-    if array[half] == value:
-        return half
-    elif half == 0:
-        return None
+def bin_search_recursive(array, what_to_find, left=0, right=None):
+    """
+    Finds element in a sorted array using recursion.
 
-    if value > array[half]:
-        return bin_search(array[half:], value)
-    else:
-        return bin_search(array[:half], value)
+    :param list array: A sorted list of values.
+    :param what_to_find: An item to find.
+
+    :returns: Index of the searchable item or -1 if not found.
+    """
+    right = right if right is not None else len(array) - 1
+    if left > right:
+        return -1  # Searchable not found
+
+    middle_pos = (left + right) // 2
+    if array[middle_pos] == what_to_find:
+        return middle_pos
+
+    if what_to_find < array[middle_pos]:
+        return bin_search_recursive(array, what_to_find, left=left, right=middle_pos - 1)
+    return bin_search_recursive(array, what_to_find, left=middle_pos + 1, right=right)
 
 
-two = bin_search(list(range(10)), 2)
-assert two == 2, "{} != 2".format(two)
+# Testing
+def test_item_should_be_found_recursive():
+    array_for_test = sorted(random.randint(0, 1000) for _ in range(100))
+    array_for_test.append(1001)  # Add testable item at the end of a test array
+    found_pos, expected_pos = bin_search_recursive(array_for_test, 1001), 100
+    assert (
+        found_pos == expected_pos
+    ), f"Wrong index position found: {found_pos}, expected: {expected_pos}"
 
-one = bin_search([1, 5, 12, 15, 19, 23, 34, 44, 44, 90], 5)
-assert one == 1, "{} != 2".format(one)
 
-no = bin_search([34, 36, 99], 40)
-assert no is None, "{} != 2".format(no)
+def test_item_not_found_in_empty_array():
+    found_pos, expected_pos = bin_search_recursive([], 1001), -1
+    assert (
+        found_pos == expected_pos
+    ), f"Found {found_pos} in empty array, the position has to be {expected_pos}"
 
-no = bin_search([], 40)
-assert no is None, "{} != 2".format(no)
+
+def test_item_should_not_be_found():
+    found_pos, expected_pos = bin_search_recursive([1, 2, 3], 4), -1
+    assert (
+        found_pos == expected_pos
+    ), f"Item 4 should not be found. Real: {found_pos}, expected: {expected_pos}"
